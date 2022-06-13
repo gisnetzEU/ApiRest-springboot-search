60bb68dd8df26c1baf8575595d1fb855fa5eb4ca
GisnetzEU <gismontero.mail@gmail.com>
2022-06-13 23:20:23 +0200

    search

---
+++ src/main/java/io/company/library/BookRepository.java
@@ -0,0 +1,16 @@
+package io.company.library;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookRepository extends CrudRepository <Book, Long> {
+    //
+    Optional<Book> findBookByTitle(String title);
+
+    List<Book> findByTitle(String title);
+    Optional<Book> deleteBookByTitle(String title);
+
+}
+
